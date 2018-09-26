56bdbad27c194246a5190f0b8c82b10c8441f8a1
victor0101 <alexandruvictorandrei@gmail.com>
2018-09-26 16:38:13 +0200

    Create assignment2.py

---
+++ assignment2.py
@@ -0,0 +1,16 @@
+x = 1
+y = 1
+z = 0
+result = 0
+
+while z < 4000000:
+   z = (x+y)         
+   if z%2 == 0:
+       result = result + z
+
+   #next iteration
+
+   x = y
+   y = z
+
+print result
